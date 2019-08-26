8710413190fe61708437ca4d00a1a7d9df1dfbd4
Bruno Marques <bruno.marques@sap.com>
2019-08-26 14:39:57 -0300

    Handling both types of image

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,29 +2,37 @@ const blockhash = require('./vendor/blockhash');
 
 let portTwitter;
 
-function loadImage(url) {
-    console.log('loadImage');
+function loadImage(message) {
     let canvas = document.createElement('canvas');
     let context = canvas.getContext('2d');
     let image = document.createElement('img');
+    document.root.appendChild(image);
+    image.crossOrigin = 'anonymous';
     image.onload = () => {
-        console.log('loadImage onload');
+        canvas.width = image.width;
+        canvas.height = image.height;
         context.drawImage(image, 0, 0);
         const imageData = context.getImageData(0, 0, image.width, image.height);
         const hash = blockhash.blockhashData(imageData, 16, 2);
         console.log(hash);
         if (hash === '074e6e1e7c1cfe08ff40fe09ec0cf809fc20fe30fe30f830ff30ff30ff708000') {
-            portTwitter.postMessage({url: url});
+            portTwitter.postMessage(message);
         }
+        image.remove();
     };
-    image.src = url;
+    switch (message.source) {
+        case "a":
+            image.src = message.attr.substring(5, message.attr.length - 2);
+            break;
+        case "img":
+            image.src = message.attr;
+            break;
+    }
 }
 
 browser.runtime.onConnect.addListener(port => {
-    console.log('background onConnect');
     portTwitter = port;
     portTwitter.onMessage.addListener(message => {
-        console.log('background onMessage');
-        loadImage(message.src);
+        loadImage(message);
     });
 });
--- app/scripts/twitter.js
+++ app/scripts/twitter.js
@@ -1,22 +1,33 @@
-const bg = browser.runtime.connect();
+const port = browser.runtime.connect();
 
 const cage = 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs2/76703971/original/1b096acf250da0eb35060f90e11098fa82bdcdcf/face-swap-you-with-nicolas-cage.jpg';
 
-bg.onMessage.addListener(message => {
-    const el = document.querySelector(`.media-img[src=${message.url}]`);
-    el.setAttribute('src', cage);
+port.onMessage.addListener(message => {
+    let el;
+    switch (message.source) {
+        case "a":
+            el = document.querySelector(`.a[data-media-entity-id=${message.entityId}]`);
+            el.style.backgroundImage = `url(${cage})`;
+            break;
+        case "img":
+            el = document.querySelector(`.a[data-media-entity-id=${message.entityId}] img`);
+            el.setAttribute('src', cage);
+            break;
+    }
+    console.log(el);
 });
 
 const observer = new MutationObserver(mutations => {
     for (const mutation of mutations) {
         for (const node of mutation.addedNodes) {
+            if (node.nodeType !== Node.ELEMENT_NODE) {
+                continue;
+            }
             for (const el of node.querySelectorAll('.media-img')) {
-                console.log('media-img');
-                bg.postMessage({src: el.getAttribute('src')});
+                port.postMessage({source: 'img', attr: el.getAttribute('src'), entityId: el.parentNode.getAttribute('data-media-entity-id')});
             }
             for (const el of node.querySelectorAll('a[rel="mediaPreview"]')) {
-                // send src to bg
-                el.style.backgroundImage = `url(${cage})`;
+                port.postMessage({source: 'a', attr: el.style.backgroundImage, entityId: el.getAttribute('data-media-entity-id')});
             }
         }
     }
