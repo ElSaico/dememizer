c477ba0836a9f03d64871b12897bc93b53b04478
Bruno Marques <bruno@bmarques.net>
2020-05-17 13:08:24 +0100

    Hashing and replacement works 100% now

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,17 +1,49 @@
 const blockhash = require('./vendor/blockhash');
 
+let replacement;
+
 function requestImage(request) {
-    return {redirectUrl: browser.runtime.getURL('images/cage.jpg')};
     let filter = browser.webRequest.filterResponseData(request.requestId);
-    const hash = blockhash.blockhashData(imageData, 16, 2);
-    console.log(hash);
-    if (hash === '074e6e1e7c1cfe08ff40fe09ec0cf809fc20fe30fe30f830ff30ff30ff708000') {
-        return {redirectUrl: browser.runtime.getURL('images/cage.jpg')};
+    let data = [];
+
+    filter.ondata = event => {
+        data.push(event.data);
+    };
+
+    // we leverage some DOM wizardry in order to get a raw bitmap from the image file
+    let canvas = document.createElement('canvas');
+    let context = canvas.getContext('2d');
+    let image = document.createElement('img');
+
+    filter.onstop = event => {
+        const blob = new Blob(data);
+        image.src = URL.createObjectURL(blob);
+        image.onload = async () => {
+            canvas.width = image.width;
+            canvas.height = image.height;
+            context.drawImage(image, 0, 0);	
+            const imageData = context.getImageData(0, 0, image.width, image.height);	
+            const hash = blockhash.blockhashData(imageData, 16, 2);
+            if (hash === 'e03fc01fc01fe03fe17ff03ff701e000c4b0bee81ecc0767023352b7dfff8003') {
+                // TODO does reading a single chunk suffice?
+                filter.write((await replacement.read()).value);
+            } else {
+                filter.write(await blob.arrayBuffer());
+            }
+
+            // cleanup, their roles are finished
+            filter.disconnect();
+            image.remove();
+            canvas.remove();
+        }
     }
 }
 
-browser.webRequest.onBeforeRequest.addListener(
-    requestImage,
-    {urls: ['https://pbs.twimg.com/media/*'], types: ["image"]},
-    ["blocking"]
-);
+fetch(browser.runtime.getURL('images/cage.jpg')).then(async response => {
+    replacement = response.body.getReader();
+    browser.webRequest.onBeforeRequest.addListener(
+        requestImage,
+        {urls: ['https://pbs.twimg.com/media/*'], types: ["image"]},
+        ["blocking"]
+    );
+});
